ecbcc7f532c45dcc4a592d4d99b7d39a3e413aab
Aditya Oberai <adityaoberai1@gmail.com>
2021-01-13 14:00:53 +0530

    Created console app in C#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>{23,45,2,1,56,3};
+            foreach(var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            myList.Sort();
+            
+            foreach(var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
